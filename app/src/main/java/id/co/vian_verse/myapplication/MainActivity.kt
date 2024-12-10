73c3dec5730e6de7d396fbc3deaecdbe2f810f27
vian <vianv31253@gmail.com>
2024-12-10 10:49:02 +0700

    Membuat Tampilan Counter

--- app/src/main/java/id/co/vian_verse/myapplication/MainActivity.kt
+++ app/src/main/java/id/co/vian_verse/myapplication/MainActivity.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -14,9 +19,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import id.co.vian_verse.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -53,10 +60,23 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello world!",
-        modifier = modifier
-    )
+    Column (
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "0",
+            style = MaterialTheme.typography.displayLarge
+        )
+        Button(
+            onClick = {},
+            modifier = Modifier.fillMaxWidth(0.5f).padding(top = 16.dp),
+            contentPadding = PaddingValues(16.dp)
+        ) {
+            Text(text = stringResource(R.string.count))
+        }
+    }
 }
 
 @Preview(showBackground = true)
